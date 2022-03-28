81d82679ba14c72b33116ff18d841cddcba8c7ac
Nate August <n.baker.august@gmail.com>
2022-03-28 11:09:53 -0400

    add fill_edge_geometry for carall loader

--- roc_bike_growth/loader.py
+++ roc_bike_growth/loader.py
@@ -2,7 +2,8 @@ import osmnx as ox
 import networkx as nx
 import numpy as np
 from roc_bike_growth.settings import CONFIG
-from shapely.geometry import Polygon, MultiPolygon
+from shapely.geometry import Polygon, MultiPolygon, LineString, Point
+
 from typing import List, Union
 
 
@@ -113,7 +114,9 @@ def bike_infra_from_polygon(
         return list(zip(custom_filters.keys(), graphs))
 
 
-def carall_from_polygon(polygon: Polygon, add_pois: bool = False) -> nx.MultiDiGraph:
+def carall_from_polygon(
+    polygon: Polygon, add_pois: bool = False, fill_edge_geometry: bool = True
+) -> nx.MultiDiGraph:
     """
     Downloads network of "driveable" roads
 
@@ -125,6 +128,9 @@ def carall_from_polygon(polygon: Polygon, add_pois: bool = False) -> nx.MultiDiG
     add_pois: bool = False
         Flag to also tag nodes that are nearest to pois identified in `download_pois`.
 
+    fill_edge_geometry: bool = True
+        Flag to fill missing edge geometries. For edge (u,v), creates LineString from u to v.
+
     Returns
     -------
     driveable network within input polygon
@@ -132,24 +138,43 @@ def carall_from_polygon(polygon: Polygon, add_pois: bool = False) -> nx.MultiDiG
 
     try:
         G = ox.graph_from_polygon(polygon, **CONFIG.osm_carall_params["carall"])
+
+        if add_pois:
+            # Download to POIs
+            pois = download_POIs(polygon)
+
+            # Find nearest node in G for each POI
+            X, Y = [], []
+            for node in pois["elements"]:
+                X.append(node["lon"])
+                Y.append(node["lat"])
+            poi_nodes = np.unique(ox.distance.nearest_nodes(G, X=X, Y=Y))
+
+            # Update those nodes to contain attribute 'poi'=True
+            update_dict = {}
+            for node in poi_nodes:
+                update_dict[node] = True
+            nx.set_node_attributes(G, update_dict, name="poi")
+
+        # Modified from https://github.com/gboeing/osmnx/blob/main/osmnx/utils_graph.py
+        if fill_edge_geometry:
+            x_lookup = nx.get_node_attributes(G, "x")
+            y_lookup = nx.get_node_attributes(G, "y")
+            eattrs = {}
+            for u, v, k, data in G.edges(keys=True, data=True):
+                if data.get("geometry", None) is None:
+                    geom = LineString(
+                        (
+                            Point((x_lookup[u], y_lookup[u])),
+                            Point((x_lookup[v], y_lookup[v])),
+                        )
+                    )
+                    eattrs[(u, v, k)] = {"geometry": geom}
+
+            nx.set_edge_attributes(G, eattrs)
+
+        return G
+
     except ox._errors.EmptyOverpassResponse:
         print(f"No OSM data for carall")
-
-    if add_pois:
-        # Download to POIs
-        pois = download_POIs(polygon)
-
-        # Find nearest node in G for each POI
-        X, Y = [], []
-        for node in pois["elements"]:
-            X.append(node["lon"])
-            Y.append(node["lat"])
-        poi_nodes = np.unique(ox.distance.nearest_nodes(G, X=X, Y=Y))
-
-        # Update those nodes to contain attribute 'poi'=True
-        update_dict = {}
-        for node in poi_nodes:
-            update_dict[node] = True
-        nx.set_node_attributes(G, update_dict, name="poi")
-
-    return G
+        return
