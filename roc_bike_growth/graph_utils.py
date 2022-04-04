1bf3b3832e33ebe962e10609caae6676988d4b00
blaisederenze <82596077+blaisederenze@users.noreply.github.com>
2022-04-04 13:20:28 -0400

    gt_with_existing_full and route_factor
    gt_with_existing_full runs the algorithm on the carall and bike_infra graphs. 
    Adds bike_infra graphs at the end. 
    Output graph edges have attributes 'generated' and 'existing' (not mutually  exclusive)

--- roc_bike_growth/graph_utils.py
+++ roc_bike_growth/graph_utils.py
@@ -1,151 +1,285 @@
 import osmnx as ox
 import networkx as nx
 from typing import List, Dict
+import igraph as ig
+
 
 def get_intersections(
-    G: nx.MultiDiGraph,
-    street_1_name: str,
-    street_2_name: str) -> List[int]:
-    '''
-    Identifies nodes in G that have edges of both street_1_name and street_2_name. 
-    
+    G: nx.MultiDiGraph, street_1_name: str, street_2_name: str
+) -> List[int]:
+    """
+    Identifies nodes in G that have edges of both street_1_name and street_2_name.
+
     Parameters
     -------
     G: MultiDiGraph
         Graph of network
     street_1_name: string
-        Street of interest, without abbreviations. Capitalization will be ignored. E.x. 'Broad Street'. 
+        Street of interest, without abbreviations. Capitalization will be ignored. E.x. 'Broad Street'.
     street_2_name: string
-        Street name for intersection. Same naming conventions as `street_1_name`. 
-        
-        
+        Street name for intersection. Same naming conventions as `street_1_name`.
+
+
     Returns
     -------
     nodes: Set
-        set of nodes where edges with name `street_1_name` intersect with edges with name `street_2_name`. 
-    '''
+        set of nodes where edges with name `street_1_name` intersect with edges with name `street_2_name`.
+    """
     street_1_name = street_1_name.lower()
     street_2_name = street_2_name.lower()
-    
+
     # Get all edges for street_name_1
     street_1_edges = {}
     street_1_nodes = []
-    for u,v in G.edges():
-        d = G.get_edge_data(u,v,0)
-        edge_name = d.get('name', '')
+    for u, v in G.edges():
+        d = G.get_edge_data(u, v, 0)
+        edge_name = d.get("name", "")
         if isinstance(edge_name, list):
-            edge_name = ' '.join(edge_name)
-            
+            edge_name = " ".join(edge_name)
+
         edge_name = edge_name.lower()
         if street_1_name in edge_name:
-            street_1_edges[(u,v)] = d
-            street_1_nodes += [u,v]
-    
+            street_1_edges[(u, v)] = d
+            street_1_nodes += [u, v]
+
     # Remove duplicate nodes
     street_1_nodes = set(street_1_nodes)
-    
+
     # Loop through edges to our street nodes to find edges with intersection street names
     street_2_nodes = []
-    for u,v in G.edges(street_1_nodes):
-        d = G.get_edge_data(u,v,0)
-        edge_name = d.get('name','')
-        if isinstance(edge_name, list): # Some street names are lists
-            edge_name=' '.join(edge_name)
+    for u, v in G.edges(street_1_nodes):
+        d = G.get_edge_data(u, v, 0)
+        edge_name = d.get("name", "")
+        if isinstance(edge_name, list):  # Some street names are lists
+            edge_name = " ".join(edge_name)
 
         edge_name = edge_name.lower()
 
         # TODO change these to regexes?
         if street_2_name in edge_name:
-            street_2_nodes += [u,v] 
+            street_2_nodes += [u, v]
         else:
             pass
-        
+
     return street_1_nodes.intersection(set(street_2_nodes))
 
-        
+
 def get_street_segment(
-    G: nx.MultiDiGraph, 
-    street_name: str, 
-    intersection_src_name: str, 
-    intersection_dest_name: str) -> list:
-    '''
+    G: nx.MultiDiGraph,
+    street_name: str,
+    intersection_src_name: str,
+    intersection_dest_name: str,
+) -> list:
+    """
     Gets the directed segements of edges with street_name that are between intersection_1_name
     and intersection_2_name. If multiple nodes are found for the intersections, the longest route
-    between any two matching intersections is returned. 
-    
+    between any two matching intersections is returned.
+
     Parameters
     -------
     G: MultiDiGraph
         Graph of network
     street_name: string
         Street of interest, without abbreviations. Capitalization will be ignored.
-        E.x. 'Broad Street'. 
+        E.x. 'Broad Street'.
     intersection_src_name: string
-        Street name for source intersection with. Same naming conventions as `street_name`. 
+        Street name for source intersection with. Same naming conventions as `street_name`.
     intersection_dest_name: str
-        Street name for dest intersection with. Same naming conventions as `street_name`. 
-        
+        Street name for dest intersection with. Same naming conventions as `street_name`.
+
     Returns
     -------
     nodes: list
-        List of nodes along the directed segement. 
-    '''
-    # TODO -- could refactor this to use get_intersections() for brevity. Current implementation works fine. 
-    
+        List of nodes along the directed segement.
+    """
+    # TODO -- could refactor this to use get_intersections() for brevity. Current implementation works fine.
+
     # Clean up names
     street_name = street_name.lower()
     intersection_src_name = intersection_src_name.lower()
     intersection_dest_name = intersection_dest_name.lower()
-    
+
     # Get all edges for street_name
     street_edges = {}
     street_nodes = []
-    for u,v in G.edges():
-        d = G.get_edge_data(u,v,0)
-        edge_name = d.get('name', '')
+    for u, v in G.edges():
+        d = G.get_edge_data(u, v, 0)
+        edge_name = d.get("name", "")
         if isinstance(edge_name, list):
-            edge_name = ' '.join(edge_name)
-            
+            edge_name = " ".join(edge_name)
+
         edge_name = edge_name.lower()
         if street_name in edge_name:
-            street_edges[(u,v)] = d
-            street_nodes += [u,v]
-    
+            street_edges[(u, v)] = d
+            street_nodes += [u, v]
+
     # Remove duplicate nodes
     street_nodes = set(street_nodes)
-    
+
     # Loop through edges to our street nodes to find edges with intersection street names
     src_nodes = []
     dest_nodes = []
-    for u,v in G.edges(street_nodes):
-        d = G.get_edge_data(u,v,0)
-        edge_name = d.get('name','')
-        if isinstance(edge_name, list): # Some street names are lists
-            edge_name=' '.join(edge_name)
+    for u, v in G.edges(street_nodes):
+        d = G.get_edge_data(u, v, 0)
+        edge_name = d.get("name", "")
+        if isinstance(edge_name, list):  # Some street names are lists
+            edge_name = " ".join(edge_name)
 
         edge_name = edge_name.lower()
 
         # TODO change these to regexes?
         if intersection_src_name in edge_name:
-            src_nodes += [u,v] 
+            src_nodes += [u, v]
         elif intersection_dest_name in edge_name:
-            dest_nodes += [u,v]
+            dest_nodes += [u, v]
         else:
             pass
-    
+
     src_intersections = street_nodes.intersection(set(src_nodes))
     dest_intersections = street_nodes.intersection(set(dest_nodes))
-    
+
     # Find the longest route between intersections in case there are multiple of either src or dest
     route = []
     for src in src_intersections:
         for dest in dest_intersections:
             # Shortest path of our subgraph will comprise all nodes in between
-            new_route = ox.distance.shortest_path(G.subgraph(street_nodes), 
-                                              src,
-                                              dest)
+            new_route = ox.distance.shortest_path(G.subgraph(street_nodes), src, dest)
             # Replace route with longest
             if len(new_route) > len(route):
                 route = new_route
-    
+
     return route
+
+def combine_nodes(    
+    dst: nx.MultiDiGraph, 
+    src: nx.MultiDiGraph, 
+    node_attributes = ['x','y','street_count'], 
+    debug = False):
+    
+    
+    ''' 
+    Adds the nodes from one graph to another.
+    
+    Parameters   
+    ------
+    dst: MultiDiGraph
+        Graph of network that edges are being added to
+    src: MultiDiGraph
+        Graph of network whose edges are added to dst 
+        
+    Returns
+    ------
+    
+    dst: 
+        Graph dst with added edges from src
+    '''
+    
+    # Get the keys for the nodes in both graphs 
+    
+    dst_nodes = list(dict(dst.nodes(data=True)).keys())
+    src_nodes = list(dict(src.nodes(data=True)).keys())
+    
+    # Add the nodes from src not in dst to dst 
+    
+    for node in src_nodes:
+        if(node in dst_nodes):
+            pass
+        else:
+            dst.add_node(node)
+            for a in node_attributes:
+                #set attributes
+                dst.nodes[node][a] = src.nodes[node][a]
+    return dst
+
+def combine_edges(
+    dst: nx.MultiDiGraph, 
+    src: nx.MultiDiGraph, 
+    debug = False):
+    
+    ''' 
+    Adds the edges from one graph to another 
+    (assuming they have the edges only contain nodes that are in the dst graph)
+    
+    Parameters   
+    ------
+    dst: MultiDiGraph
+        Graph of network that edges are being added to
+    src: MultiDiGraph
+        Graph of network whose edges are added to dst 
+        
+    Returns
+    ------
+    
+    dst: 
+        Graph dst with added edges from src
+    '''
+    
+    
+    # Get the keys for the values between edges (would need to use multiple edges 
+    
+    src_edges = list(src.edges)
+    dst_edges = list(dst.edges)
+    
+    # Add edges not in dst to dst from src 
+
+    for edge in src_edges:
+        if(edge in dst_edges):
+            pass
+        else:
+            attr = dict(src[edge[0]][edge[1]][edge[2]])
+            dst.add_edge(edge[0],edge[1])
+            edge_attr = {edge: attr}
+            if(debug):
+                print(edge_a)
+            nx.set_edge_attributes(dst,edge_attr)
+    return dst
+
+def ig_to_nx(
+    G_ig: ig.Graph
+    ):
+    
+    #Inits empty networkx MultiDiGraph
+    
+    G_nx = nx.MultiDiGraph()
+    
+    for node in G_ig.vs:
+        #The networkx key values are called _nx_name in iGraph 
+        
+        key = node['_nx_name']
+        
+        #adds the node attributes of the iGraph node to new node in nx at key of node
+        
+        G_nx.add_node(key)
+        a = node.attributes()
+        del a['_nx_name']
+        attrs = {key: a}
+        
+        nx.set_node_attributes(G_nx, attrs)
+        
+    for edge in G_ig.es:
+        
+        #Get the keys of the edge in terms of nx keys
+        
+        src_i = edge.source
+        trg_i = edge.target
+        src = G_ig.vs[src_i]['_nx_name']
+        trg = G_ig.vs[trg_i]['_nx_name']
+        
+        #add the edges to the nx graph with attributes
+
+        G_nx.add_edge(src, trg)
+        a = edge.attributes()
+        attrs = {(src,trg,0 ): a}
+        
+        nx.set_edge_attributes(G_nx,attrs)
+
+    #Sets the graph attributes
+    
+    for a in G_ig.attributes():
+        
+        G_nx.graph[a] = G_ig[a]
+    
+    return G_nx
+
+    def graph_union(G_1, G_2):
+        return [0,0,0,0]
--- roc_bike_growth/paper_gt.py
+++ roc_bike_growth/paper_gt.py
@@ -8,31 +8,32 @@ import math
 import random
 import time
 import pickle as pk
+import roc_bike_growth.graph_utils as gu
 
-#Algorithm the same as the one in their code with some unnessecary bits  removed. 
-#Enumerates all of the connections between points of interest in graph and sums over their weights
+# Algorithm the same as the one in their code with some unnessecary bits  removed.
+# Enumerates all of the connections between points of interest in graph and sums over their weights
 def get_poipairs_by_distance(G, pois_indices):
     # Get sequences of nodes and edges in shortest paths between all pairs of pois
     poi_nodes = []
     poi_edges = []
     for c, v in enumerate(pois_indices):
-        # Possible cost parameters we could add to weight could be calculated here. 
-        #We'd have to implement a "get shortest paths  weighted on accident data etc." 
-        # could also include node weights 
-        poi_nodes.append(G.get_shortest_paths(v, pois_indices[c:], output = "vpath"))
-        poi_edges.append(G.get_shortest_paths(v, pois_indices[c:], output = "epath"))
+        # Possible cost parameters we could add to weight could be calculated here.
+        # We'd have to implement a "get shortest paths  weighted on accident data etc."
+        # could also include node weights
+        poi_nodes.append(G.get_shortest_paths(v, pois_indices[c:], output="vpath"))
+        poi_edges.append(G.get_shortest_paths(v, pois_indices[c:], output="epath"))
 
     # Sum up weights (distances) of all paths
     poi_dist = {}
     for paths_n, paths_e in zip(poi_nodes, poi_edges):
         for path_n, path_e in zip(paths_n, paths_e):
             # Sum up distances of path segments from first to last node
-            #would have to use distances incorporating parameters if we were to use them here. 
-            path_dist = sum([G.es[e]['weight'] for e in path_e])
+            # would have to use distances incorporating parameters if we were to use them here.
+            path_dist = sum([G.es[e]["weight"] for e in path_e])
             if path_dist > 0:
-                poi_dist[(path_n[0],path_n[-1])] = path_dist
+                poi_dist[(path_n[0], path_n[-1])] = path_dist
 
-    temp = sorted(poi_dist.items(), key = lambda x: x[1])
+    temp = sorted(poi_dist.items(), key=lambda x: x[1])
     # Back to ids
     poipairs = []
     for p in temp:
@@ -40,59 +41,80 @@ def get_poipairs_by_distance(G, pois_indices):
 
     return poipairs
 
-def greedy_triangulation(GT, poipairs, prune_factor = 1, prune_measure = "betweenness"):
-    for poipair, poipair_distance in poipairs:
-            try:
-                poipair_ind = (GT.vs.find(id = poipair[0]).index, GT.vs.find(id = poipair[1]).index)
-            except:
-                print(poipair)
-                for v in GT.vs:
-                    print(v['id'])
-                
-            if not new_edge_intersects(GT, (GT.vs[poipair_ind[0]]["x"], GT.vs[poipair_ind[0]]["y"], GT.vs[poipair_ind[1]]["x"], GT.vs[poipair_ind[1]]["y"])):
-                GT.add_edge(poipair_ind[0], poipair_ind[1], weight = poipair_distance)
 
-        # Get the measure for pruning
-    #if prune_measure == "betweenness":
-    BW = GT.edge_betweenness(directed = False, weights = "weight")
-    #here is where we can include some other attributes: for instance BW = betweenness/log(accident likelihood)
-    qt = np.quantile(BW, 1-prune_factor)
+def greedy_triangulation(GT, poipairs, prune_factor=1, prune_measure="betweenness"):
+    for poipair, poipair_distance in poipairs:
+        try:
+            poipair_ind = (
+                GT.vs.find(id=poipair[0]).index,
+                GT.vs.find(id=poipair[1]).index,
+            )
+        except:
+            print(poipair)
+            for v in GT.vs:
+                print(v["id"])
+
+        if not new_edge_intersects(
+            GT,
+            (
+                GT.vs[poipair_ind[0]]["x"],
+                GT.vs[poipair_ind[0]]["y"],
+                GT.vs[poipair_ind[1]]["x"],
+                GT.vs[poipair_ind[1]]["y"],
+            ),
+        ):
+            GT.add_edge(poipair_ind[0], poipair_ind[1], weight=poipair_distance)
+
+    # Get the measure for pruning
+    # if prune_measure == "betweenness":
+    BW = GT.edge_betweenness(directed=False, weights="weight")
+    # here is where we can include some other attributes: for instance BW = betweenness/log(accident likelihood)
+    qt = np.quantile(BW, 1 - prune_factor)
     sub_edges = []
     for c, e in enumerate(GT.es):
-        if BW[c] >= qt: 
+        if BW[c] >= qt:
             sub_edges.append(c)
-            #we only keep edge c if it is in the pf percentile of edges in our metric (0 = no edges, 1 = all edges from GT)
+            # we only keep edge c if it is in the pf percentile of edges in our metric (0 = no edges, 1 = all edges from GT)
         GT.es[c]["bw"] = BW[c]
-        GT.es[c]["width"] = math.sqrt(BW[c]+1)*0.5
+        GT.es[c]["width"] = math.sqrt(BW[c] + 1) * 0.5
     GT = GT.subgraph_edges(sub_edges)
     return GT
 
 
- # Get node pairs we need to route, sorted by distance
-# allows us to only includ relevant pairs in 
-def route_node_pairs(G,GT):
+# Get node pairs we need to route, sorted by distance
+# allows us to only includ relevant pairs in
+def route_node_pairs(G, GT, route_factor):
     routenodepairs = {}
     for e in GT.es:
         routenodepairs[(e.source_vertex["id"], e.target_vertex["id"])] = e["weight"]
-    routenodepairs = sorted(routenodepairs.items(), key = lambda x: x[1])
+    routenodepairs = sorted(routenodepairs.items(), key=lambda x: x[1])
 
     # Do the routing
     GT_indices = set()
+
+    for e in G.es:
+        e['mod_weight'] = e['weight'] - e['weight']*route_factor*int(e['existing'] == True)
+    
     for poipair, poipair_distance in routenodepairs:
-        poipair_ind = (G.vs.find(id = poipair[0]).index, G.vs.find(id = poipair[1]).index)
-        sp = set(G.get_shortest_paths(poipair_ind[0], poipair_ind[1], weights = "weight", output = "vpath")[0])
+        poipair_ind = (G.vs.find(id=poipair[0]).index, G.vs.find(id=poipair[1]).index)
+        sp = set(
+            G.get_shortest_paths(
+                poipair_ind[0], poipair_ind[1], weights='mod_weight', output="vpath"
+            )[0]
+        )
         GT_indices = GT_indices.union(sp)
 
     GT_final = G.induced_subgraph(GT_indices)
     return GT_final
 
-#the below classes are ripped from the code. its an intersection function which could probably be optimized better but it definitely works :)
+
+# the below classes are ripped from the code. its an intersection function which could probably be optimized better but it definitely works :)
 class MyPoint:
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        
-        
+
+
 def new_edge_intersects(G, enew):
     """Given a graph G and a potential new edge enew,
     check if enew will intersect any old edge.
@@ -106,29 +128,72 @@ def new_edge_intersects(G, enew):
             return True
     return False
 
-def segments_intersect(A,B,C,D):
+
+def segments_intersect(A, B, C, D):
     """Check if two line segments intersect (except for colinearity)
     Returns true if line segments AB and CD intersect properly.
     Adapted from: https://stackoverflow.com/questions/3838329/how-can-i-check-if-two-segments-intersect
     """
-    if (A.x == C.x and A.y == C.y) or (A.x == D.x and A.y == D.y) or (B.x == C.x and B.y == C.y) or (B.x == D.x and B.y == D.y): return False # If the segments share an endpoint they do not intersect properly
-    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+    if (
+        (A.x == C.x and A.y == C.y)
+        or (A.x == D.x and A.y == D.y)
+        or (B.x == C.x and B.y == C.y)
+        or (B.x == D.x and B.y == D.y)
+    ):
+        return False  # If the segments share an endpoint they do not intersect properly
+    return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
 
-def ccw(A,B,C):
-    return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
 
-#craete a deepcopy of the original graph with no edges
+def ccw(A, B, C):
+    return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x)
 
-#def greedy_triangulation_subgraph(G, pois_indices = [], pois_method = pass):
-def gt_from_scratch(G, pois_indices, prune_factor = 1):
-    
+
+# craete a deepcopy of the original graph with no edges
+
+def set_all_edge_attributes(G_src,G_dst, atr_name):
+    edges = list(G_src.edges)
+    atr = {}
+    for edge in edges:
+        atr[edge] =  True
+    nx.set_edge_attributes(G_dst, atr, atr_name)
+    return G_dst
+
+
+# def greedy_triangulation_subgraph(G, pois_indices = [], pois_method = pass):
+def gt_from_scratch(G, pois_indices, route_factor =  0 , prune_factor=1):
+
     G_temp = copy.deepcopy(G)
-    for e in G_temp.es: # delete all edges
+    for e in G_temp.es:  # delete all edges
         G_temp.es.delete(e)
     GT = copy.deepcopy(G_temp.subgraph(set(pois_indices)))
     poipairs = get_poipairs_by_distance(G, pois_indices)
-    #print(poipairs)
-    GT = greedy_triangulation(GT, poipairs)
-    GT_final = route_node_pairs(G,GT)
+    # print(poipairs)
+    GT = greedy_triangulation(GT, poipairs, prune_factor)
+    GT_final = route_node_pairs(G, GT, route_factor)
     return GT_final
 
+def gt_with_existing_full(G_base, G_existing, route_factor = 0, prune_factor = 1):
+    
+    
+    G_comb_nx = gu.combine_nodes(G_base,G_existing)
+    G_comb_nx = gu.combine_edges(G_comb_nx,G_existing)
+
+    G_comb_nx = set_all_edge_attributes(G_existing, G_comb_nx, 'existing')
+    
+    G_comb = ig.Graph.from_networkx(G_comb_nx)
+
+    for i,v in enumerate(G_comb.vs):
+        v['id'] = i
+    for e in G_comb.es:
+        e['weight'] = e['length']
+    pois_ids = [v_index for v_index,vertex in enumerate(G_comb.vs) if vertex['poi']]
+    G_gen = gt_from_scratch(G_comb, pois_ids, route_factor, prune_factor)
+    G_nx = gu.ig_to_nx(G_gen)
+    G_nx = gu.combine_nodes(G_nx,G_existing)
+    G_nx = gu.combine_edges(G_nx,G_existing)
+    
+    G_nx = set_all_edge_attributes(G_existing, G_nx, 'existing')
+    G_nx = set_all_edge_attributes(gu.ig_to_nx(G_gen), G_nx, 'generated')
+    
+    return G_nx
+    
